cb403877866a4061c420cb7ddf73032f2081e712
Abdul-Awul-Arman <abdulawularman52114@gmail.com>
2025-05-28 10:36:37 +0600

    learnt date

--- Date.sql
+++ Date.sql
@@ -2,6 +2,31 @@ show timezone;
 
 SELECT now();
 
-CREATE Table timez(ts  TIMESTAMP without time zone,tsz TIMESTAMP with time zone);
+
+CREATE Table TimeZone(ts TIMESTAMP without time zone,tsz TIMESTAMP with time zone);
+
+INSERT into TimeZone VALUES('2024-01-12 10:45:00','2024-01-12 10:45:00');
+
+SELECT * FROM TimeZone;
+
+SELECT CURRENT_DATE;
+
+-- this call casting
+SELECT now()::time without time zone;
+SELECT now()::time WITH time zone;
+SELECT now()::date;
+
+-- custom date 
+SELECT to_char(now(),'yyyy/mm/dd');
+
+SELECT CURRENT_DATE -INTERVAL '1 year 1 month';
+
+SELECT *,age(CURRENT_DATE,dob) from students;
+
+SELECT extract(YEAR from '2024-02-4'::date);
+
+SELECT 'y'::BOOLEAN;
+
+
 
   
